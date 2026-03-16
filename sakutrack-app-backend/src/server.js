3fbe68319a8db0f0760fefa9a96685d682ac5e7a
NannFth <adnandfathir123@gmail.com>
2026-03-16 21:57:22 +0700

    Perubahan backend untuk AWS

--- sakutrack-app-backend/src/server.js
+++ sakutrack-app-backend/src/server.js
@@ -22,7 +22,8 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 
 app.listen(port, () => {
-  console.log(`Server SakuTrack berjalan di ${port}`);
+  console.log(`Server running at http://${host}:${port}`);
 });
