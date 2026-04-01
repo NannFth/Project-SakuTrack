1f500b5994c2068255f0bd8660dcce399cdaf3e6
NannFth <adnandfathir123@gmail.com>
2026-04-01 20:57:44 +0700

    Deploy

--- sakutrack-app-frontend/src/connection.js
+++ sakutrack-app-frontend/src/connection.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const connection = axios.create({
-  baseURL: 'http://54.255.51.12:3000/api',
+  baseURL: '/api',
 });
 
 const getValidToken = () => {
